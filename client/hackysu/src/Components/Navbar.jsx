22f2eae668212832403e57ac532a0cde4ca24c0d
toslickforya <slickethan@Icloud.com>
2024-04-06 13:45:23 -0400

    My honeybun

--- client/hackysu/src/Components/Navbar.jsx
+++ client/hackysu/src/Components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Flex, Box, Text, Button } from '@chakra-ui/react';
-
+import { Flex, Box, Text, Button, Image } from '@chakra-ui/react';
 function Navbar() {
   return (
     <Flex
@@ -8,25 +7,23 @@ function Navbar() {
       align="center"
       justify="space-between"
       padding="1rem"
-      bg="blue.500"
-      color="white"
+      bg="blue.900"
+      color="gold"
     >
-      <Box>
-        <Text fontSize="xl" fontWeight="bold">
-          Your Logo
-        </Text>
+      <Box maxW="33px">
+         <Image src="Subject.png" alt="Logo" boxSize="50px" /> 
       </Box>
       <Box>
-        <Button variant="link" mr="4">
+        <Button variant="link" mr="4" color ='gold'>
           Home
         </Button>
-        <Button variant="link" mr="4">
+        <Button variant="link" mr="4" color ='gold'>
           About
         </Button>
-        <Button variant="link" mr="4">
+        <Button variant="link" mr="4" color ='gold'>
           Services
         </Button>
-        <Button variant="link">Contact</Button>
+        <Button variant="link" color = 'gold'>Contact</Button>
       </Box>
     </Flex>
   );
